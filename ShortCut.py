fe714ca5ae84b6b11c1345a406f54a01c6a3ca3c
Salem Raouf <salamasalem84@gmail.com>
2023-04-26 20:53:15 +0100

    refactor: clean code

--- ShortCut.py
+++ ShortCut.py
@@ -1,39 +1,51 @@
-import os 
-from Tkinter import *
-root = Tk()
+import os
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
 root.title('SHORTCUT')
-root.iconbitmap(default='add.ico')
-Pic = PhotoImage(file="add.gif")
 root.geometry('200x60+0+200')
 root.configure(background='#000000')
-#Define The widgets  
-val = StringVar()
-name = Entry(root,width=60,textvariable=val)
+root.iconbitmap(default='add.ico')
 
-#set functions
-def set_file():
-    get_name = val.get()
-    os.mkdir('./%s' % (get_name))
-    txt = open('msg.txt','r')
-    data = txt.read()
-    myFile = open(get_name + "/index.html", "w")
-    myFile.write(data)
-    js = os.mkdir(os.path.join(str(get_name),'js'))
-    myFile = open(get_name + "/js/app.js", "w")
-    css = os.mkdir(os.path.join(str(get_name),'css'))
-    myFile = open(get_name + "/css/style.css", "w")
+# Define the widgets
+val = tk.StringVar()
+name = tk.Entry(root, width=60, textvariable=val)
+button = tk.Button(root)
+button.config(background="#000000", image=tk.PhotoImage(file="add.gif"))
 
-button = Button(root, image=Pic,command=set_file)
+# Set functions
+def create_files():
+    # Create a new folder with the name entered in the text entry
+    folder_name = val.get()
+    os.mkdir(f'./{folder_name}')
+    
+    # Copy files to the new folder
+    with open('msg.txt', 'r') as file:
+        data = file.read()
+        with open(f'{folder_name}/index.html', 'w') as index_file:
+            index_file.write(data)
+        
+        os.mkdir(os.path.join(str(folder_name), 'js'))
+        with open(f'{folder_name}/js/app.js', 'w') as js_file:
+            pass
+        
+        os.mkdir(os.path.join(str(folder_name), 'css'))
+        with open(f'{folder_name}/css/style.css', 'w') as css_file:
+            pass
 
-def passText(event):
-    name.delete(0, END)
-    passcheck=True
-#widgets config style    
-button.config(background="#000000")
-button.pack(side=LEFT)
-name.config(background="#000000",foreground="#9acd32")
+def clear_text(event):
+    # Clear the text entry when the user clicks on it
+    name.delete(0, tk.END)
+
+# Widget configuration
+name.config(background="#000000", foreground="#9acd32")
+name.insert(0, "Name Your Folder")
+name.bind("<Button>", clear_text)
+
+button.config(command=create_files)
+button.pack(side=tk.LEFT)
 name.pack(ipady=20)
-root.overrideredirect(0)
-name.insert(0,"Name Your Folder")
-name.bind("<Button>",passText)
+
+# Start the main event loop
 root.mainloop()
